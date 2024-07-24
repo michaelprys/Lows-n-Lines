7cd9e0805311a25e1dba5655ae87fda869086d16
michaelprys <musha09cz@gmail.com>
2024-07-24 22:44:09 +0200

    added: page about

---
+++ app/pages/about.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <section class="container mb-24 mt-16 w-full max-w-[1260px]">
+      <h1 class="text-center font-['Gin-Test'] text-3xl uppercase">
+        Welcome to Lows 'n' Lines lowrider showroom & service center
+      </h1>
+      <NuxtImg
+        class="mx-auto mt-14 object-cover"
+        src="/images/about/car-1.jpg"
+        width="1260"
+        height="628"
+        alt="car image"
+      />
+      <div class="mt-8">
+        <h3 class="text-xl font-bold">Showroom: <i>Buy, Sell, Trade</i></h3>
+        <p class="mt-4">
+          For the dedicated lowrider enthusiast, we offer an extensive selection
+          of meticulously crafted lowriders and classic cars. With over 75
+          stunning lowriders and trucks in our inventory, we invite you to
+          explore and trade your beloved vehicles with us. We offer easy
+          financing options to make your dream ride a reality. Additionally, we
+          buy lowriders and classic cars (25 years or older) as well as select
+          newer special interest collector cars.
+        </p>
+        <p class="mt-4">
+          Our strength lies in our marketing prowess. We believe that every
+          lowrider has a unique story to tell—stories of the people who build,
+          own, and cherish them. Our dynamic exhibitions, art, books,
+          collectibles, and other cool stuff attract locals, tourists, and
+          repeat visitors alike. Our extensive online advertising across
+          multiple channels, including Hemmings, Facebook, eBay, Craigslist, and
+          targeted platforms, ensures a wide audience sees our inventory. We
+          have proudly sold over 1,000 cars to clients around the world.
+        </p>
+        <h3 class="mt-4 text-xl font-bold">
+          Service Center: <i>Old or New We Fix It For You</i>
+        </h3>
+        <p class="mt-4">
+          Our full-service center is staffed by experienced lowrider
+          professionals. We offer comprehensive auto repair, detailing,
+          appraisals, storage (short or long term), and transport services.
+          Whether your car is old or new, our service center can handle it all.
+          We also arrange convenient pick-up and drop-off services.
+        </p>
+      </div>
+      <h2 class="mt-12 text-center font-['Gin-Test'] text-3xl">
+        Lowrider Museum
+      </h2>
+      <NuxtImg
+        class="mx-auto mt-14 object-cover"
+        src="/images/about/car-2.jpg"
+        width="1260"
+        height="684"
+        alt="car image"
+      />
+      <p class="mt-8">
+        <strong>The Lows 'n' Lines Lowrider Museum</strong> celebrates the rich
+        history and culture of lowriders, showcasing how they have influenced
+        and been influenced by American and global trends. We aim to bring these
+        stories to life through changing exhibitions, period artifacts,
+        multimedia presentations, and special events. Our commitment to
+        community outreach includes learning and fundraising opportunities for
+        students, clubs, and non-profit organizations, encouraging the next
+        generation of lowrider enthusiasts. Our Museum Store offers a variety of
+        items for the car lover in your life, from handmade toys and car books
+        to fun vintage finds. Open Monday-Saturday from 10am-5pm. Admission is
+        $12 per person for visitors aged 7 and older.
+      </p>
+      <p class="mt-4">
+        Lows 'n' Lines, LLC is an Equal Opportunity Employer. We are a welcoming
+        and inclusive business for our clients, visitors, employees, and
+        vendors. We support diversity, equity, and inclusion in everything we
+        do. We do not discriminate based on race, color, gender, gender
+        identity, gender expression, orientation, disability, religion, age,
+        national origin, veteran status, or any other status protected under
+        federal, state, or local law. We encourage individuals from all
+        protected classes to apply to our growing business.
+      </p>
+      <p class="mt-4">
+        We are proud to have been named Business of the Year. Visit us at 2564
+        Portland Road (US Route 1), Arundel, Maine. We're just 90 minutes north
+        of Boston and 20 minutes south of Portland, Maine. Our hours are
+        Monday-Saturday from 8:30am-5pm. Call us anytime at 207.602.6620 or
+        email us at info@lowsnlines.com.
+      </p>
+    </section>
+  </div>
+</template>
